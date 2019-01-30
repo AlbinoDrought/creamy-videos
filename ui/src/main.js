f2b11b83c6af113b72a74db6b1106da2dff3cc75
AlbinoDrought <albinodrought@gmail.com>
2019-01-29 22:54:54 -0800

    feat: dump info about repo and source download to console

--- ui/src/main.js
+++ ui/src/main.js
@@ -7,6 +7,14 @@ import './global.css';
 
 Vue.config.productionTip = false;
 
+console.log(`
+# AlbinoDrought/creamy-videos
+
+Repo: https://github.com/AlbinoDrought/creamy-videos
+
+Source: https://${window.location.host}/source.tar.gz
+`);
+
 new Vue({
   router,
   store,
