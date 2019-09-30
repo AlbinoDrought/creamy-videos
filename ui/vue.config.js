bca0cbbc2a5717f39f024484829d56d73f92b069
Sean <sean@albinodrought.com>
2019-09-30 00:05:28 -0700

    chore: initial PWA config

---
+++ ui/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pwa: {
+    name: 'Creamy Videos',
+    themeColor: '#1b1b1b',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      appleTouchIcon: 'icons/icon-152x152.png',
+      msTileImage: 'icons/icon-144x144.png'
+    },
+  }
+};
